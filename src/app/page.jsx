6c25e8d7c16ff1138c9736a1c55fdaeb7c9da590
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-02-20 07:23:22 +0530

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,17 @@
 import { useState } from "react"
 import { MdOutlineUploadFile } from "react-icons/md";
 import { TypeAnimation } from "react-type-animation";
+import Loader from "./components/loader";
 export default function Home() {
   
 const [file, setFile] = useState()
 const [text, setText] = useState()
 const [data, setData] = useState()
+const [loading, setLoading] = useState(false)
+const [generating, setGenerating] = useState(false)
 
  const Generate =async () => {
+  setLoading(true)
    if (!text){ 
     alert("Enter prompt")
     return;}
@@ -23,9 +27,9 @@ const [data, setData] = useState()
     });
     if (res.ok) {
       const resp = await res.json()
-      console.log(resp)
       const concatenatedString = resp.result.join(' ');
       setData(concatenatedString)
+      setFile(null)
     }
     
   //  if(data.ok) return window.location.href="/result";
@@ -39,10 +43,11 @@ const [data, setData] = useState()
     <div className="h-[85vh] w-[100vw] relative px-8">
 
   <div className="textarea absolute w-[90%] bottom-5 overflow-clip">
+  {loading&&<div><Loader/></div>}
  {data&&<TypeAnimation
       sequence={[
         data, 
-        () => {
+        () => { setLoading(false)
           console.log('Sequence completed');
         },
       ]}
@@ -54,10 +59,10 @@ const [data, setData] = useState()
   </div>
 
     </div>
-    <div className="flex justify-between items-center w-[98%] border-2 border-blue-400 p-1 rounded-2xl">
+    <div className="flex justify-between items-center w-[80%] border-2 border-blue-400 p-1 rounded-2xl">
    <div className="h-12 w-[calc(100%-200px)]"> <input placeholder="Enter your prompt"  type="text" className=" pl-2 bg-transparent  focus:outline-none h-full w-full" onChange={e => setText(e.target.value)} /></div>
-   <div className="h-12  relative hover:text-blue-500 overflow-hidden rounded-full w-12"> <input className="h-full opacity-0 w-full" type="file" onChange={e => setFile(e.target?.files[0])} /><MdOutlineUploadFile className="absolute top-3 left-2 text-black pointer-events-none" size={30}/></div>
-    <button className="h-12 w-36 rounded-2xl bg-blue-600" onClick={Generate}>
+   <div className="h-12  relative overflow-hidden rounded-full w-12"> <input className="h-full opacity-0 w-full" type="file" onChange={e => setFile(e.target?.files[0])} /><MdOutlineUploadFile className={`absolute top-3 left-2  ${file?'text-blue-500':'text-black'}  pointer-events-none`} size={30}/></div>
+    <button className="h-12 w-36 font-semibold text-white rounded-2xl bg-blue-500" onClick={Generate}>
       Generate
     </button>
     </div>
